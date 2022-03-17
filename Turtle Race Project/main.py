7e2698d1db99f6d0c2fcb43fb34ae9ef0fd73da0
Antonia Andreou <antoniaandreou@gmail.com>
2022-03-17 23:45:53 +0000

    Added docstrings for functions and renamed some variables to be more self-explanatory

--- Turtle Race Project/main.py
+++ Turtle Race Project/main.py
@@ -1,8 +1,22 @@
+"""
+Created By: Antonia Andreou
+Created Date: 17th March 2022
+Revised By: Antonia Andreou
+Revised Date: 17th March 2022
+"""
+
 from turtle import Turtle, Screen
 import random
 
 
+# noinspection GrazieInspection
 def turtle_racers(color: str, y_pos: int) -> object:
+    """
+    Creates turtle objects.
+    :param color: Pass the colour for the turtle
+    :param y_pos: Set the y coordinate of the turtle racer's starting position
+    :return: The new turtle object
+    """
     turtle_racer = Turtle(shape="turtle")
     turtle_racer.color(color)
     turtle_racer.penup()
@@ -10,7 +24,13 @@ def turtle_racers(color: str, y_pos: int) -> object:
     return turtle_racer
 
 
+# noinspection GrazieInspection
 def turtle_speed(t_racer: object):
+    """
+    Allocates a random speed & steps for the turtle objects.
+    :param t_racer: The turtle object created outside this function
+    :return: Same object but with speed and forward allocated to it.
+    """
     random_speed = random.randint(1, 9)
     t_racer.speed(random_speed)
     random_move = random.randint(1, 4)
@@ -36,11 +56,11 @@ user_choice = s.textinput(title="Choose a turtle", prompt="(Red, Blue, Green, Or
 turtle_colours = ["red", "blue", "green", "orange", "cyan", "pink"]
 
 # Increment
-distance = [-150, -90, -30, 30, 90, 150]
+y_cor_position = [-150, -90, -30, 30, 90, 150]
 
 # Turtle Racers
 turtle_objects = []
-for c, d in zip(turtle_colours, distance):
+for c, d in zip(turtle_colours, y_cor_position):
     turtle_objects.append(turtle_racers(c, d))
 
 # Main Race
