b0430898a478d3c7a18d8843701fdcb7d596e9e4
Antonia Andreou <antoniaandreou@gmail.com>
2022-03-29 19:38:28 +0100

    update main program with snake extension and own body collision

--- Intermediate Projects/Snake Game Project/main.py
+++ Intermediate Projects/Snake Game Project/main.py
@@ -28,10 +28,26 @@ while game_on:
     time.sleep(0.1)
     snake.snake_move()
 
+    # Detection of food & snake growth
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.snake_extend()
         score += 1
 
+    # Score update
     score_board.score_increase(score=score)
+
+    # Detection of wall collision
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_on = False
+        score_board.game_over()
+
+    # Detection of own body collision
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_on = False
+            score_board.game_over()
 
 screen.exitonclick()
