be4b78c4fe75bfacdad786f012ca7e3d0c23ad4c
Antonia Andreou <antoniaandreou@gmail.com>
2022-04-02 04:51:08 +0100

    update food class with docstrings and constants

--- Intermediate Projects/Snake Game Project/food.py
+++ Intermediate Projects/Snake Game Project/food.py
@@ -1,21 +1,51 @@
+'''
+Created by: Antonia Andreou
+Created Date: 30th March 2022
+Amended By: Antonia Andreou
+Amended Date: 2nd April 2022
+'''
+
+# Modules
 from turtle import Turtle
 import random
 
+# Class Constants
+SPEED = "fastest"
+STARTING_NUMBER = -270
+ENDING_NUMBER = 270
+SIZE = 10
+COLOUR = "yellow"
 
 class Food(Turtle):
+    """
+    A class to represent the snake food for the game.
+    ...
+    Attributes
+    ----------
+
+    Methods
+    -------
+    refresh:
+        Creates a new random position for the food.
+    """
 
     def __init__(self):
+        """
+        Constructs the snake food for the game.
+        """
         super().__init__()
         self.penup()
         self.hideturtle()
-        self.speed("fastest")
+        self.speed(SPEED)
         self.refresh()
 
     def refresh(self):
+        """
+        Clears the current food from the screen and displays it again at a new random position.
+        :return: None
+        """
         self.clear()
-        x_coordinate = random.randint(-270, 270)
-        y_coordinate = random.randint(-270, 270)
+        x_coordinate = random.randint(STARTING_NUMBER, ENDING_NUMBER)
+        y_coordinate = random.randint(STARTING_NUMBER, ENDING_NUMBER)
         self.goto(x=x_coordinate, y=y_coordinate)
-        self.dot(10, "yellow")
-
-
+        self.dot(SIZE, COLOUR)
