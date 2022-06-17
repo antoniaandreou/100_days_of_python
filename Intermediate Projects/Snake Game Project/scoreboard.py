d7937814ca7e33cbd9d2f4c73d1368187d8f29fe
Antonia Andreou <antoniaandreou@gmail.com>
2022-06-17 23:02:52 +0100

    add scoreboard initialization, score tracking, highscore tracking and reset methods to scoreboard class

--- Intermediate Projects/Snake Game Project/scoreboard.py
+++ Intermediate Projects/Snake Game Project/scoreboard.py
@@ -24,13 +24,23 @@ class ScoreBoard(Turtle):
     Attributes
     ----------
 
-    score : int
+    current_score : int
         To pass the current score for the game.
+    high_score: int
+        To pass and track the highest score achieved.
 
     Methods
     -------
+    scoreboard_initialization:
+        Creates the scoreboard object.
+    score_tracking:
+        Tracks the current score.
+    highscore_tracking:
+        Keeps record of the highest score achieved.
     score_increase:
         Refreshes the score on display.
+    scoreboard_reset:
+        Clears the current score and re-initiates the scoreboard object.
     game_over:
         Displays a message when the game has finished.
     """
@@ -40,20 +50,53 @@ class ScoreBoard(Turtle):
         Constructs the scoreboard display for a game.
         """
         super().__init__()
+        self.current_score = 0
+        self.high_score = 0
+
+    def scoreboard_initialization(self):
+        """
+        Creates the scoreboard object for the game.
+        :return: None
+        """
         self.penup()
         self.color(COLOUR)
         self.hideturtle()
         self.goto(X_COORDINATE, Y_COORDINATE)
-        self.score_increase(score=0)
 
-    def score_increase(self, score: int):
+    def score_tracking(self):
         """
-        Clears the screen and displays the score passed in the function.
-        :param score: integer input of current score
+        Tracks the current score for the game.
+        :return: None
+        """
+        self.current_score += 1
+
+    def highscore_tracking(self):
+        """
+        Keeps track of the highest score achieved in the game.
+        :return: None
+        """
+        if self.current_score > self.high_score:
+            self.high_score = self.current_score
+
+    def score_increase(self):
+        """
+        Clears the screen and displays the score and high score variables passed in the write function.
         :return: None
         """
         self.clear()
-        self.write(arg=f"Score: {score}", move=False, align=ALIGNMENT, font=FONT)
+        self.write(arg=f"Score: {self.current_score} Highscore: {self.high_score}"
+                   , move=False, align=ALIGNMENT, font=FONT)
+
+    def scoreboard_reset(self):
+        """
+        Clears current score and re-initiates the scoreboard for a game restart.
+        :return:
+        """
+        self.clear()
+        self.scoreboard_initialization()
+        self.highscore_tracking()
+        self.current_score = 0
+        self.score_increase()
 
     def game_over(self):
         """
