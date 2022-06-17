330cf18cc9cd372b98a39427a60352e105c622e5
Antonia Andreou <antoniaandreou@gmail.com>
2022-06-17 23:04:07 +0100

    add the functionality to continue playing or not by resetting the snake and scoreboard objects but keeping track of the highscore or exiting the game.

--- Intermediate Projects/Snake Game Project/main.py
+++ Intermediate Projects/Snake Game Project/main.py
@@ -3,7 +3,7 @@
 Created by: Antonia Andreou
 Created Date: 30th March 2022
 Amended By: Antonia Andreou
-Amended Date: 1st April 2022
+Amended Date: 17th June 2022
 '''
 
 # Modules
@@ -13,6 +13,15 @@ from food import Food
 from scoreboard import ScoreBoard
 import time
 
+
+def controls():
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
+
 # Settings
 screen = Screen()
 screen.title("Antonia's Snake Game")
@@ -20,42 +29,56 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 
+# Object creation
 snake = Snake()
 food = Food()
 score_board = ScoreBoard()
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-
 # MAIN
-score = 0
 game_on = True
 while game_on:
+
+    # Initiate game
+    controls()
     screen.update()
     time.sleep(0.1)
     snake.snake_move()
+    score_board.scoreboard_initialization()
 
     # Detection of food & snake growth
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.snake_extend()
-        score += 1
+        score_board.score_tracking()
+        score_board.highscore_tracking()
 
     # Score update
-    score_board.score_increase(score=score)
+    score_board.score_increase()
 
     # Detection of wall collision
     if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_on = False
-        score_board.game_over()
+        # Pop up prompt
+        player_continue = screen.textinput("Would you like to play again?", "(y/n)").lower()
+        if player_continue == 'y':
+            controls()
+            score_board.scoreboard_reset()
+            snake.snake_reset()
+        else:
+            score_board.game_over()
+            game_on = False
 
     # Detection of own body collision
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_on = False
-            score_board.game_over()
+            # Pop up prompt
+            player_continue = screen.textinput("Would you like to play again?", "(y/n)").lower()
+            if player_continue == 'y':
+                controls()
+                score_board.scoreboard_reset()
+                snake.snake_reset()
+            else:
+                score_board.game_over()
+                game_on = False
+
 
 screen.exitonclick()
