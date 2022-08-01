6deb249c918ec92bd771fecc6c7153d5aadd9ab4
Antonia Andreou <antoniaandreou@gmail.com>
2022-08-01 20:29:09 +0100

    update to handle key errors

--- Intermediate Projects/NATO Alphabet Project/main.py
+++ Intermediate Projects/NATO Alphabet Project/main.py
@@ -4,8 +4,13 @@ nato_alphabet = pd.read_csv('nato_phonetic_alphabet.csv')
 
 nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
-user_input = input("Enter a word: ")
-
-letter_phonetics = [nato_alphabet_dict[letter.upper()] for letter in user_input]
-print(letter_phonetics)
+program_on = True
+while program_on:
+    user_input = input("Enter a word: ")
+    try:
+        letter_phonetics = [nato_alphabet_dict[letter.upper()] for letter in user_input]
+        print(letter_phonetics)
+        program_on = False
+    except KeyError:
+        print("Only letters in the alphabet can be used!")
 
